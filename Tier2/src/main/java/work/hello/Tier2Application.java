8285ac364816a2373312d4876332e157d882bde9
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2021-10-30 18:48:52 +0200

    Init

---
+++ Tier2/src/main/java/work/hello/Tier2Application.java
@@ -0,0 +1,13 @@
+package work.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tier2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tier2Application.class, args);
+    }
+
+}
---
+++ Tier2/src/test/java/work/hello/Tier2ApplicationTests.java
@@ -0,0 +1,13 @@
+package work.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
