21a8cfcfadd1c94748a3fd5c789a665dcacb55d2
gof3ck69 <sorryy647@gmail.com>
2021-10-31 14:54:33 +0100

    ApplyToJobListing page and Job Listings

--- Tier1/WebApplication/WebApplication/obj/Debug/net5.0/RazorDeclaration/Pages/CreateJobListing.razor.g.cs
+++ Tier1/WebApplication/WebApplication/obj/Debug/net5.0/RazorDeclaration/Pages/CreateJobListing.razor.g.cs
@@ -13,84 +13,84 @@ namespace WebApplication.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 1 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 2 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 3 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 4 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 5 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 6 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 7 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 8 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 9 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using WebApplication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
+#line 10 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\_Imports.razor"
 using WebApplication.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
+#line 2 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
 using WebApplication.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
+#line 3 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
 using WebApplication.Data;
 
 #line default
@@ -105,7 +105,7 @@ using WebApplication.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 19 "C:\Users\coste\Desktop\School\3rd Semester\SEP3\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
+#line 19 "C:\Users\Tudor\RiderProjects\hello-work\Tier1\WebApplication\WebApplication\Pages\CreateJobListing.razor"
            
         private JobListing jobListing = new JobListing();
 
