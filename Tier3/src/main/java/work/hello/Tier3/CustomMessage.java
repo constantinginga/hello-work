35b95b979a424b722000922315b5857790a1c329
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2021-10-31 11:25:43 +0100

    init tier3

---
+++ Tier3/src/main/java/work/hello/Tier3/CustomMessage.java
@@ -0,0 +1,20 @@
+package work.hello.Tier3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CustomMessage {
+
+    private String messageId;
+    private String message;
+    private Date messageDate;
+
+}
