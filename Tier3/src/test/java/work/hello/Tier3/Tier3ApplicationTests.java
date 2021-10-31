a17830597b37ace8681769f9da6f6833e40b9223
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2021-10-31 11:07:28 +0100

    init tier3

---
+++ Tier3/src/test/java/work/hello/Tier3/Tier3ApplicationTests.java
@@ -0,0 +1,13 @@
+package work.hello.Tier3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
