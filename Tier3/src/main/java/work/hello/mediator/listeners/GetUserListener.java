cd2d858d47c218b6e24e551c0bbf83ba1adc919d
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2021-11-11 08:55:28 +0100

    tryed adding git ignore

--- Tier3/src/main/java/work/hello/mediator/listeners/GetUserListener.java
+++ Tier3/src/main/java/work/hello/mediator/listeners/GetUserListener.java
@@ -4,13 +4,17 @@ import work.hello.mediator.MessageListenerRPC;
 import work.hello.model.data.CustomMessage;
 import work.hello.model.data.User;
 
-public class GetUserListener extends MessageListenerRPC {
-    public GetUserListener() {
-        super("getUser", "getUser");
-    }
+public class GetUserListener extends MessageListenerRPC
+{
+  public GetUserListener()
+  {
+    super("getUser", "getUser");
+  }
 
-    @Override
-    public String handleResponse(CustomMessage message) {
-        return super.getGson().toJson(super.getModel().getUser(getGson().fromJson(message.getContent(), User.class)));
-    }
+  @Override public String handleResponse(CustomMessage message)
+  {
+    System.out.println(message.getContent());
+    return super.getGson().toJson(super.getModel()
+        .getUser(getGson().fromJson(message.getContent(), User.class)));
+  }
 }
