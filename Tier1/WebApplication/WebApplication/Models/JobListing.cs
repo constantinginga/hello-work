3c02e7f5b61ff4b434c2142dfc4fb86f2cbd463f
gof3ck69 <sorryy647@gmail.com>
2021-11-16 17:51:35 +0100

    Forgot

--- Tier1/WebApplication/WebApplication/Models/JobListing.cs
+++ Tier1/WebApplication/WebApplication/Models/JobListing.cs
@@ -7,11 +7,35 @@ using System.Threading.Tasks;
 namespace WebApplication.Models
 {
     public class JobListing
-    {
+    { 
         [Required]
         public int JobId { get; set; }
         [Required]
-        public string Details { get; set; }
+        public string JobTitle { get; set; }
+        
+        [Required]
+        public string JobType { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required, Range(0, int.MaxValue, ErrorMessage = "Invalid job experience")]
+        public float ExperienceLevel { get; set; }
+
+        
+        public List<string> JobRequirments { get; set; }
+        
+       
+        public List<string> JobPrivilleges { get; set; }
+        
+        [RegularExpression(@"(\+?[0-9]{2})\s*[0-9]{2}\s*[0-9]{2}\s*[0-9]{2}\s*[0-9]{2}", ErrorMessage = "Invalid phone number or character!")]
+        public string PhoneNumber { get; set; }
+        
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid character!")]
+        public string Email { get; set; }
+        
+        
+        public string ApplicationDeadline { get; set; }
 
     }
 }
