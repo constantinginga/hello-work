fb1bfa92ad8858d23bfb763cc6c9276c1fe6bd90
Constantin Ginga <costelgn@gmail.com>
2021-11-17 21:22:20 +0100

    Add job listing filtering & Fix overflow issue

---
+++ Tier1/WebApplication/WebApplication/Models/JobSchedule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+
+    static public class JobSchedule
+    {
+        public enum Schedule
+        {
+            [Description("Full time")] FullTime,
+            [Description("Part time")] PartTime,
+            [Description("Internship")] Internship,
+            [Description("Volunteering")] Volunteering
+        }
+
+        public static string ToDescription<T>(this T value) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("Not an enum");
+            }
+            var fieldName = Enum.GetName(typeof(T), value);
+            if (fieldName == null)
+            {
+                return string.Empty;
+            }
+            var fieldInfo = typeof(T).GetField(fieldName, BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Static);
+            if (fieldInfo == null)
+            {
+                return string.Empty;
+            }
+            var descriptionAttribute = (DescriptionAttribute)fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault();
+            if (descriptionAttribute == null)
+            {
+                return fieldInfo.Name;
+            }
+            return descriptionAttribute.Description;
+        }
+    }
+
+}
