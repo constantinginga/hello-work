f565494b4e502866667d74973d4cff6fa8e8719e
KlausBenislavskis <klavs.benislavskis@inbox.lv>
2021-11-23 16:57:13 +0100

    testr

---
+++ Tier1/WebApplication/WebApplication/Data/Applications/ApplicationData-DESKTOP-JO1DGT5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebApplication.Models;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace WebApplication.Data.Applications
+{
+    public class ApplicationData : IApplicationData
+    {
+        private WebApiUtil _client;
+        private const string _url = "http://localhost:8082/application";
+
+        public IList<Application> Applications { get; private set; }
+
+        public ApplicationData()
+        {
+            _client = new(_url);
+            Applications = new List<Application>();
+        }
+
+        public async Task<Application> AddApplication(Application application)
+        {
+            // store locally
+            Applications.Add(application);
+
+            // store in web api
+            string applicationJson = JsonSerializer.Serialize(application);
+            string response = await _client.Post("", applicationJson);
+            Application applicationResponse = JsonSerializer.Deserialize<Application>(response,
+                new JsonSerializerOptions()
+                {
+                });
+            return applicationResponse;
+        }
+
+        public async Task<IList<Application>> GetApplications()
+        {
+             
+             string responese = await _client.Get("");
+             Applications = JsonSerializer.Deserialize<List<Application>>(responese, new JsonSerializerOptions
+             {
+                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+             });
+             return Applications;
+        }
+
+        public async Task UpdateApplication(Application application)
+        {
+            var toUpdateS = JsonSerializer.Serialize(application);
+            var responseMessage = await _client.Patch("",toUpdateS);
+            Console.Write(responseMessage);
+        }
+
+    }
+}
